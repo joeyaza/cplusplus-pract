2aa001d5134d5b78384ab00d63f8382f3a241647
jazadehdel <jazadehdel@sovrn.com>
2018-01-12 17:11:31 +0000

    first commit

---
+++ Fraction.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Header.h
+ * Author: jazadehdel
+ *
+ * Created on 12 January 2018, 17:00
+ */
+
+#ifndef FRACTION_H
+#define FRACTION_H
+
+
+class Fraction {
+private:
+    int den;
+    int num;
+    
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: jazadehdel
+ *
+ * Created on 02 January 2018, 11:26
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
